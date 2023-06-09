901171f49d0203be632fd804e389ebfd045f2493
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-06-09 15:11:52 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Your App',
+        short_name: 'App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+    assetsDir: '',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
